6ff6c1fee5f8ee163c13eca3ee76186fd0be5892
Salman <salman@clustox.com>
2015-03-03 15:15:23 +0500

    removed objects

---
+++ public/assets/details-box-3959f294de23ef4f1e81ac93e6f9fab0.js
@@ -0,0 +1,1 @@
+function showDetailsBox(e){e.fadeIn(300)}function closeDetailsBox(){$(".details-box").fadeOut(300)}$(function(){$(".details-box .details-overlay, .details-box .details-close-button").live("click",function(){closeDetailsBox()})});
---
+++ public/assets/vendors/devise/registrations/company-details-921501ca76ab3fc7d8535bd4618d8e98.js
@@ -0,0 +1,1 @@
+$(function(){$("#describe-continue").click(function(){var e="-624px";$("#company-registration #step2_init").animate({marginLeft:e},300,"swing"),$("#company-registration #step2_optional").slideDown("slow")})});
