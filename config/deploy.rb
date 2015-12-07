ed6d03ade22d612848197e0878f9b2e05513c870
Hisham Malik <hisham@arkhitech.com>
2015-12-08 01:52:56 +0500

    sorting deployment scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,73 @@
+set :stages, ['staging', 'production']
+set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
-#require 'rvm/capistrano'
-#require "whenever/capistrano"
-#require "delayed/recipes"
+require "whenever/capistrano"
+require "rvm/capistrano"
 
-set :application, 'muddleme'
-#set :stages, %w(production staging)
-#set :default_stage, 'production'
+set :branch, ENV['BRANCH'] || ENV['REVISION'] || ENV['WERCKER_GIT_COMMIT'] || 'master'
 
-#ssh stuff
-set :user, 'developer'
-set :password, 'PLMMFX502'
-set :port, 22
+set :application, "muddleme"
 set :scm, :git
-set :use_sudo, false
-default_run_options[:pty] = true
-set :default_shell, '/bin/bash -l'
-set :repository, 'git@bitbucket.org:kevinbrothers/muddleme_final.git'
-set :deploy_via, :remote_cache
-#set :branch, 'pepperjam_integration'
-#set :rails_env, 'production'
-#set :deploy_to,   "/home/#{user}/#{application}_#{rails_env}"
-set :shared_children, shared_children + %w{public/system}
+set :repository, "git@bitbucket.org:kevinbrothers/muddlemenewfork3-27-2015.git"
+set :deploy_to, "/var/www/muddleme"
+#set :deploy_via, :remote_cache
+set :deploy_env, 'production'
+ssh_options[:forward_agent] = true
 
-# will be different entries for app, web, db if you host them on different servers
-#server 'muddleme.com', :app, :web, :primary => true
+set :whenever_command, "bundle exec whenever"
 
-set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
+namespace :delayed_job do
+  def delayed_job_command
+    fetch(:delayed_job_command, 'bin/delayed_job')
+  end
+end
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
+
+namespace :db do
+  desc "Create database YAML in shared path"
+  task :default do
+    section = { "adapter" => "mysql2", "database"=>app_db_database ,"host" => app_db_host, "username" => app_db_username, "password" => app_db_password, "encoding" => "utf8"}
+    db_config = {}
+    ["development", "test", "production"].each do |env|
+      db_config[env] = section
+    end
+
+    run "mkdir -p #{shared_path}/config"
+    put db_config.to_yaml, "#{shared_path}/config/database.yml"
+  end
+
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after  'deploy:setup', "db:default"
+#before 'deploy:assets:precompile', "db:symlink"
+after "deploy:finalize_update", "db:symlink"
+
+# Migrations
+after "deploy", "deploy:migrate"
+
+#after "deploy:update_code", "db:default"
+#after "deploy:update_code", "db:symlink"
+
+namespace :passenger do
+  desc "Restart Passenger"  
+  task :restart do  
+    run "touch #{current_path}/tmp/restart.txt"  
+  end
+end
+after :deploy, "passenger:restart"
+
 namespace :deploy do
-  after 'deploy:restart', 'deploy:cleanup'
-  task :restart, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-
-  after 'deploy:update_code' do
-    run "cd #{release_path} && #{try_sudo} RAILS_ENV=#{rails_env} bundle exec whenever --clear-crontab #{application}"
-    run "cd #{release_path} && #{try_sudo} RAILS_ENV=#{rails_env} bundle exec whenever --update-crontab #{application}"
-  end
-
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
       else
         logger.info "Skipping asset pre-compilation because there were no asset changes"
@@ -49,3 +75,78 @@ namespace :deploy do
     end
   end
 end
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
+
+# social.yml
+namespace :social do
+  desc "Create social YAML in shared path"
+
+  task :default do
+    config = {
+        "production" => {
+            'fb_app_id'          => fb_app_id,
+            'fb_app_secret'      => fb_app_secret,
+            'tw_consumer_key'    => tw_consumer_key,
+            'tw_consumer_secret' => tw_consumer_secret,
+            'g_consumer_key'     => g_consumer_key,
+            'g_consumer_secret'  => g_consumer_secret,
+            'paypal_login'       => paypal_login, 
+            'paypal_password'    => paypal_password,
+            'paypal_signature'   => paypal_signature, 
+            'authorize_login'    => authorize_login,
+            'authorize_password' => authorize_password,
+            'merchant_production_mode'   => merchant_production_mode
+        }
+    }
+    run "mkdir -p #{shared_path}/config"
+    put config.to_yaml, "#{shared_path}/config/social.yml"
+  end
+
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/social.yml #{release_path}/config/social.yml"
+  end
+end
+after  'deploy:setup', :social
+#before 'deploy:assets:precompile', "social:symlink"
+after "deploy:finalize_update", "social:symlink"
+
+# hostname.yml
+namespace :hostname do
+  desc "Create hostname YAML in shared path"
+
+  task :default do
+    config = {
+        "production" => {
+            'hostname' => hostname_hostname,
+            'full_hostname' => hostname_full_hostname
+        }
+    }
+    run "mkdir -p #{shared_path}/config"
+    put config.to_yaml, "#{shared_path}/config/hostname.yml"
+  end
+
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/hostname.yml #{release_path}/config/hostname.yml"
+  end
+end
+
+namespace :deploy do
+  desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles, :roles => :app do
+    #run "cd #{release_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+  end
+end
+
+after("deploy:update_code", "deploy:build_missing_paperclip_styles")
+
+after  'deploy:setup', :hostname
+#before 'deploy:assets:precompile', "hostname:symlink"
+after "deploy:finalize_update", "hostname:symlink"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,18 +1,15 @@
-#server "108.171.184.148", :app, :web, :db, :primary => true
-server "166.78.107.196", :app, :web, :db, :primary => true
+server "104.197.68.95", :app, :web, :db, :primary => true
 
-#set :user, "application"
+set :user, "muddleme"
 set :use_sudo, false
 set :environment, "production"
 set :rails_env, "production"
-set :branch, 'pepperjam_integration'
-set :deploy_to,   "/home/#{user}/#{application}_#{rails_env}"
 
 
-set :app_db_database, 'application'
+set :app_db_database, 'muddleme'
 set :app_db_host, "localhost"
-set :app_db_username, "application"
-set :app_db_password, "uov8ieDu"
+set :app_db_username, "muddleme"
+set :app_db_password, "Arkh!t3ch"
 
 set :fb_app_id, '444018178973901'
 set :fb_app_secret, 'a61c8b011bf6353250161840def2c670'
@@ -22,10 +19,10 @@ set :g_consumer_key, '648763776892.apps.googleusercontent.com'
 set :g_consumer_secret, 'e37lEFUQcWdZ7UHVwkXrapwu'
 
 set :paypal_login, 'kevin_api1.muddleme.com'
-set :paypal_password, 'FJSUHS44CVCMT6KN'
+set :paypal_password, 'FJSUHS44CVCMT6KN' 
 set :paypal_signature, 'AFcWxV21C7fd0v3bYYYRCpSSRl31A6PbmCghMzJ3F-vAxF38niR-SXqv'
 set :authorize_login, '6Y5g6H4Uun'
-set :authorize_password, '5bB6be6436uH7aCE'
+set :authorize_password, '5bB6be6436uH7aCE' 
 set :merchant_production_mode, true
 
 set :hostname_hostname, 'muddleme.com'
